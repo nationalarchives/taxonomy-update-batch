0518395f32822024f5aeb7490cff0d7a5a4005ef
Jeremie Charlet <Jeremie.Charlet@nationalarchives.gsi.gov.uk>
2016-08-01 11:16:50 +0100

    project initialised with SPRING INITIALIZR

---
+++ src/main/java/uk/gov/nationalarchives/discovery/taxonomy/TaxonomyUpdateBatchApplication.java
@@ -0,0 +1,12 @@
+package uk.gov.nationalarchives.discovery.taxonomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxonomyUpdateBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxonomyUpdateBatchApplication.class, args);
+	}
+}
